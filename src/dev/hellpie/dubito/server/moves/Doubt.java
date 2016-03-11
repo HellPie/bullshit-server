a3900a0688ebf7e84fb2793859233e84321d981e
HellPie <hellpiedev+github@gmail.com>
2016-03-11 22:01:16 +0100

    Basic types. Initial commit.

---
+++ src/dev/hellpie/dubito/server/moves/Doubt.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 Diego Rossi (@_HellPie)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.hellpie.dubito.server.moves;
+
+import dev.hellpie.dubito.server.types.Card;
+import dev.hellpie.dubito.server.types.CardNumber;
+import dev.hellpie.dubito.server.types.Player;
+
+import java.util.ArrayList;
+
+public class Doubt implements IMove {
+	@Override
+	public boolean onChosen(ArrayList<Card> cardsOnTable, Player player, CardNumber numberOnTable) {
+		return false;
+	}
+}
